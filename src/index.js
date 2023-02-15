c7a5f1b0df974bff4e644597064c043d2142655e
veronika447 <starenkaya.veronika@gmail.com>
2023-02-15 20:36:31 +0300

    feat: add function brascets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = Array.from(str);
+  let a;
+  do {
+    a = arr.length;
+    bracketsConfig.forEach(item => {
+      for (let i = 0; i < arr.length; i++) {
+        if (item[0] == arr[i] && item[1] == arr[i + 1])
+          arr.splice(i, 2);
+      }
+    })
+  } while (a > arr.length)
+  return arr.length === 0 ? true : false;
 }
